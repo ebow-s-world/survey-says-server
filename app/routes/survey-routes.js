cbdb4eab5ad9d7fe32adf946a31a294aa4a0188d
cande04 <cory.anders.atc@gmail.com>
2019-06-26 12:30:46 -0400

    Create CRUD methods for surveys
    1) add survey routes to server.js
    2) created Index, Create, Destroy, and Update methods for surveys
    3) created curl scripts to test CRUD methods
    4) successfully CRUD-ed surveys using curl scripts

---
+++ app/routes/survey-routes.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const Survey = require('../models/survey')
+const router = express.Router()
+
+const passport = require('passport')
+const requireToken = passport.authenticate('bearer', { session: false })
+const removeBlanks = require('../../lib/remove_blank_fields')
+
+const customErrors = require('../../lib/custom_errors')
+const handle404 = customErrors.handle404
+const requireOwnership = customErrors.requireOwnership
+
+// create
+router.post('/surveys', requireToken, (req, res, next) => {
+  req.body.survey.owner = req.user.id
+
+  Survey.create(req.body.survey)
+    .then(survey => {
+      res.status(201).json({ survey: survey.toObject() })
+    })
+    .catch(next)
+})
+
+// index
+router.get('/surveys', requireToken, (req, res, next) => {
+  Survey.find()
+    .then(surveys => {
+      return surveys.map(survey => survey.toObject())
+    })
+    .then(surveys => res.status(200).json({ survey: surveys }))
+    .catch(next)
+})
+
+// DESTROY
+router.delete('/surveys/:id', requireToken, (req, res, next) => {
+  Survey.findById(req.params.id)
+    .then(handle404)
+    .then(survey => {
+      requireOwnership(req, survey)
+      survey.remove()
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+// update
+router.patch('/surveys/:id', requireToken, removeBlanks, (req, res, next) => {
+  delete req.body.survey.owner
+
+  Survey.findById(req.params.id)
+    .then(handle404)
+    .then(survey => {
+      requireOwnership(req, survey)
+      return survey.update(req.body.survey)
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+module.exports = router
